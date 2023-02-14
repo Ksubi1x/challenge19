caff16488e6ebdb3c591d95a5d75963dd22dfbea
Ksubi 1k <xksubi1x@gmail.com>
2023-02-14 17:06:39 -0600

    update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,6 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 
 export const putDb = async (content) => {
+
   console.log('PUT to the database');
 
   const jateDb = await openDB('jate', 1);
@@ -28,6 +29,7 @@ export const putDb = async (content) => {
   const result = await request;
 
   console.log('🚀 - data saved to the database', result);
+
 }
 // TODO: Add logic for a method that gets all the content from the database
 
@@ -48,6 +50,7 @@ export const getDb = async () => {
   console.log('result.value', result);
   
   return result;
+  
 };
 
 initdb();
